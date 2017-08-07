0c0da9cc12c7fae92bfe7735165fd0ff027a7d2a
Christopher Rackauckas <accounts@chrisrackauckas.com>
2017-08-07 08:05:17 -0700

    trigger

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,4 @@ using Base.Test
 @time @testset "Unrolled Tests" begin include("ode_unrolled_comparison_tests.jl") end
 @time @testset "Feagin Tests" begin include("ode_feagin_tests.jl") end
 @time @testset "Units Tests" begin include("units_tests.jl") end
+
