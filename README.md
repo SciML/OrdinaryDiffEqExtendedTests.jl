67fa56fc81e060616326d7155569b36d9e84571c
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-02-20 05:37:37 -0800

    migrate a bunch of tests

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 
 This repository is for extended tests of OrdinaryDiffEq.jl. They cover extra
 features like full convergence tests of the Feagin methods and units compatibility
-which take too long to be part of the standard test suite.
+which take too long to be part of the standard test suite. Additionally, specific
+features tested for modeling domains are added here. If you have a downstream
+package and would like to make sure some very specific functionality is
+tested, feel free to add a PR here. 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,19 @@
 using OrdinaryDiffEqExtendedTests, OrdinaryDiffEq
 using Base.Test
 
-@time @testset "Init dt vs dorpri tests" begin include("init_dt_vs_dopri_tests.jl") end
-@time @testset "Unrolled Tests" begin include("ode_unrolled_comparison_tests.jl") end
+const CACHE_TEST_ALGS = [Euler(),Midpoint(),RK4(),SSPRK22(),SSPRK33(),SSPRK53(),
+  SSPRK63(),SSPRK73(),SSPRK83(),SSPRK432(),SSPRK932(),SSPRK54(),SSPRK104(),CarpenterKennedy2N54(),
+  BS3(),BS5(),DP5(),DP5Threaded(),DP8(),Feagin10(),Feagin12(),Feagin14(),TanYam7(),
+  Tsit5(),TsitPap8(),Vern6(),Vern7(),Vern8(),Vern9(),OwrenZen3(),OwrenZen4(),OwrenZen5()]
+
 @time @testset "Feagin Tests" begin include("ode_feagin_tests.jl") end
 @time @testset "Units Tests" begin include("units_tests.jl") end
+@time @testset "Time Derivative Tests" begin include("time_derivative_test.jl") end
+@time @testset "Jacobian Tests" begin include("jacobian_tests.jl") end
+@time @testset "Number Type Tests" begin include("ode_numbertype_tests.jl") end
+@time @testset "Ndim Complex Tests" begin include("ode_ndim_complex_tests.jl") end
+@time @testset "In-Place Tests" begin include("ode_inplace_tests.jl") end
+@time @testset "Adaptive Tests" begin include("ode_adaptive_tests.jl") end
 @time @testset "PSOS and Energy Conserve" begin include("psos_and_energy_conservation.jl") end
+@time @testset "Init dt vs dorpri tests" begin include("init_dt_vs_dopri_tests.jl") end
+@time @testset "Unrolled Tests" begin include("ode_unrolled_comparison_tests.jl") end
